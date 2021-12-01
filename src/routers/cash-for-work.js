3d50f4cc196436227d45bbc306664d4332731a2d
marwacoder <49866172+marwacoder@users.noreply.github.com>
2021-12-01 06:02:48 +0100

    change route

--- src/routers/cash-for-work.js
+++ src/routers/cash-for-work.js
@@ -11,7 +11,7 @@ const CashForWorkController = require("../controllers/CashForWorkController");
 
 // router.use(Auth);
 router.post("/vendor_task/evidence", VendorAuth, FileValidator.checkTaskProgressEvidenceFile(), CashForWorkController.uploadProgreeEvidenceVendor);
-router.post("/beneficiary_field_agent/evidence", FieldAgentAuth, FileValidator.checkTaskProgressEvidenceFile(), CashForWorkController.uploadProgreeEvidenceFieldAgent);
+router.post("/field_agent_task/evidence", FieldAgentAuth, FileValidator.checkTaskProgressEvidenceFile(), CashForWorkController.uploadProgreeEvidenceFieldAgent);
 router.post("/beneficiary_task/evidence", BeneficiaryAuth,  VendorBeneficiaryAuth , FileValidator.checkTaskProgressEvidenceFile(), CashForWorkController.uploadProgreeEvidenceByBeneficiary);
 router.post("/beneficiary_task/pick",BeneficiaryAuth, VendorBeneficiaryAuth, CashForWorkController.pickTaskFromCampaign);
 
