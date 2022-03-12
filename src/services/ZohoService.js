efa2430a7039d6cca91ebbb21811a8aeaefcc43f
Jibril Muhammad <jibrilmohammed39@gmail.com>
2022-03-12 22:48:43 +0000

    Update src/services/ZohoService.js

--- src/services/ZohoService.js
+++ src/services/ZohoService.js
@@ -9,10 +9,10 @@ class ZohoService {
     return new Promise(async (resolve, reject) => {
       try {
        const {data} = await Axios.post(`https://accounts.zoho.com/oauth/v2/token?response_type=authorization_code&client_id=${zohoCrmConfig.clientID}&client_secret=${zohoCrmConfig.clientSecret}&redirect_uri=https://chats.vercel.app`)
-        console.log(data, 'data 1')
+        
        resolve(data)
       }catch(error) {
-        console.log(error.response)
+        
         reject(error);
       }
     });
@@ -24,7 +24,7 @@ class ZohoService {
         const refresh = await this.generateOAuthToken()
         const refresh_token = refresh.refresh_token
        const {data} = await Axios.post(`https://accounts.zoho.com/oauth/v2/token?${refresh_token}&${zohoCrmConfig.clientID}&${zohoCrmConfig.clientSecret}&scope=Desk.tickets.READ,Desk.basic.READ&redirect_uri=https://chats.vercel.app&grant_type=${refresh_token}`)
-       console.log(data, 'data 2') 
+       
        resolve(data)
       }catch(error) {
         console.log(error.response)
@@ -43,7 +43,7 @@ class ZohoService {
             'Authorization': `Zoho-oauthtoken ${access_token}`
           }
        })
-       console.log(data, 'data 3')
+       
         resolve(data)
       }catch(error) {
         console.log(error.response)
