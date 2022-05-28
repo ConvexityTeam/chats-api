8c9f6673fedb4f0ef9d9e360e295ad1a3e8f563f
marwacoder <jibrilmohammed39@gmail.com>
2022-05-28 01:24:06 +0100

    duplicate

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -35,7 +35,13 @@ class ProductService {
     ));
   }
 
-
+static async findProduct(where){
+  return Product.findAll({
+    where: {
+      ...where
+    }
+  })
+}
 
 
   static findCampaignProducts(CampaignId) {
