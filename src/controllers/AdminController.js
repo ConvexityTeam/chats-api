9753a2f007949eef256efae3535e7b93e616f763
marwacoder <jibrilmohammed39@gmail.com>
2022-06-26 01:04:23 +0530

    profile update

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -72,7 +72,7 @@ static async verifyAccount (req, res){
       gender: "required|in:male,female",
       address: "string",
       location: "string",
-      dob: ['regex:/^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/'],
+      dob: "string",
       phone: ['required','regex:/^([0|\+[0-9]{1,5})?([7-9][0-9]{9})$/'],
       country: "required|string",
       currency: 'required|string',
