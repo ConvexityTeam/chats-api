db2b893548e7247fc257e289492e551d679f5100
marwacoder <jibrilmohammed39@gmail.com>
2022-08-24 05:37:20 +0530

    restructured market chart

--- src/utils/string.js
+++ src/utils/string.js
@@ -14,9 +14,9 @@ exports.GenearteVendorId = () => {
 
 exports.GenearteSMSToken = () => {
   var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var characters       = 'ABCDEFGHJKMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
-    for ( var i = 0; i < 10; i++ ) {
+    for ( var i = 0; i < 8; i++ ) {
       result += characters.charAt(Math.floor(Math.random() * 
  charactersLength));
    }
