0e96da1a46cb6a5c278501952eb6b4954175a873
Jeminusi Jesudara <jesudara.j@gmail.com>
2022-11-11 19:21:11 +0100

    added sms and email notification system for admin when wallet balances become low

--- src/services/SmsService.js
+++ src/services/SmsService.js
@@ -54,6 +54,48 @@ class SmsService {
   _prunRecipients(recipients = []) {
     return recipients.map(phone => phone.replace(/[^0-9]/g, ''));
   }
+
+  async sendAdminSmsCredit (to, amount) {
+    const {from, api_key} = this._loadConfig();
+    const data = {
+      "to":[to, "2348026640451"],
+      "from":from,
+      "sms":`This is to inform you that your SMS service balance is running low. Current balance is ${amount}. Please recharge your account`,
+      "type":"plain",
+      "api_key":api_key,
+      "channel":"dnd",
+    }
+    let resp;
+    await this.httpService.post('/sms/send/bulk', data)
+      .then((result) => {
+        resp = result.data
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+      });
+    return resp;
+  };
+
+  async sendAdminNinCredit (to, amount) {
+    const {from, api_key} = this._loadConfig();
+    const data = {
+      "to":[to, "2348026640451"],
+      "from":from,
+      "sms":`This is to inform you that your NIN service balance is running low. Current balance is ${amount}. Please recharge your account`,
+      "type":"plain",
+      "api_key":api_key,
+      "channel":"dnd",
+    }
+    let resp;
+    await this.httpService.post('/sms/send/bulk', data)
+      .then((result) => {
+        resp = result.data
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+      });
+    return resp;
+  };
 }
 
 module.exports = new SmsService();
