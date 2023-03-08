148efbfb23ef9a52d86d28a7457d86d2a22205d2
marwacoder <jibrilmohammed39@gmail.com>
2023-03-08 05:59:38 +0100

    added campaign to transaction

--- src/services/TransactionService.js
+++ src/services/TransactionService.js
@@ -5,14 +5,21 @@ const {Op} = require('sequelize');
 class TransactionService {
   static async findOrgnaisationTransactions(
     OrganisationId,
-    extraClause = null,
+    extraClause = null
   ) {
     return Transaction.findAll({
       where: {
         ...extraClause,
-        OrganisationId,
+        OrganisationId
       },
-      attributes: ['reference','amount', 'status', 'transaction_type', 'createdAt', 'updatedAt'],
+      attributes: [
+        'reference',
+        'amount',
+        'status',
+        'transaction_type',
+        'createdAt',
+        'updatedAt'
+      ],
       include: [
         {
           model: Wallet,
@@ -24,8 +31,8 @@ class TransactionService {
               as: 'User',
               attributes: userConst.publicAttr,
               attributes: []
-            },
-          ],
+            }
+          ]
         },
         {
           model: Wallet,
@@ -37,11 +44,11 @@ class TransactionService {
               as: 'User',
               attributes: userConst.publicAttr,
               attributes: []
-            },
-          ],
-        },
+            }
+          ]
+        }
       ],
-      order: [['createdAt', 'DESC']],
+      order: [['createdAt', 'DESC']]
     });
   }
 
@@ -57,9 +64,9 @@ class TransactionService {
             {
               model: User,
               as: 'User',
-              attributes: userConst.publicAttr,
-            },
-          ],
+              attributes: userConst.publicAttr
+            }
+          ]
         },
         {
           model: Wallet,
@@ -69,11 +76,11 @@ class TransactionService {
             {
               model: User,
               as: 'User',
-              attributes: userConst.publicAttr,
-            },
-          ],
-        },
-      ],
+              attributes: userConst.publicAttr
+            }
+          ]
+        }
+      ]
     });
   }
 
@@ -81,7 +88,7 @@ class TransactionService {
     return Transaction.findAll({
       where,
       attributes: [[Sequelize.fn('SUM', Sequelize.col('amount')), 'total']],
-      raw: true,
+      raw: true
     });
   }
 
@@ -89,8 +96,8 @@ class TransactionService {
     return Transaction.findAll({
       where: {
         OrganisationId,
-        transaction_type: 'transfer',
-      },
+        transaction_type: 'transfer'
+      }
     });
   }
 
@@ -98,11 +105,10 @@ class TransactionService {
     return Transaction.findAll({
       where: {
         BeneficiaryId,
-        transaction_type: 'spent',
-      },
+        transaction_type: 'spent'
+      }
     });
   }
-
 
   static async getAllTransactions() {
     try {
@@ -125,15 +131,15 @@ class TransactionService {
     try {
       const TransactionToUpdate = await Transaction.findOne({
         where: {
-          id: Number(id),
-        },
+          id: Number(id)
+        }
       });
 
       if (TransactionToUpdate) {
         await Transaction.update(updateTransaction, {
           where: {
-            id: Number(id),
-          },
+            id: Number(id)
+          }
         });
 
         return updateTransaction;
@@ -149,8 +155,8 @@ class TransactionService {
     try {
       const theTransaction = await Transaction.findOne({
         where: {
-          id: Number(id),
-        },
+          id: Number(id)
+        }
       });
 
       return theTransaction;
@@ -166,10 +172,10 @@ class TransactionService {
           [Op.or]: [
             {
               BeneficiaryId: id,
-              VendorId: id,
-            },
-          ],
-        },
+              VendorId: id
+            }
+          ]
+        }
       });
 
       return theTransaction;
@@ -181,15 +187,15 @@ class TransactionService {
     try {
       const TransactionToDelete = await Transaction.findOne({
         where: {
-          id: Number(id),
-        },
+          id: Number(id)
+        }
       });
 
       if (TransactionToDelete) {
         const deletedTransaction = await Transaction.destroy({
           where: {
-            id: Number(id),
-          },
+            id: Number(id)
+          }
         });
         return deletedTransaction;
       }
