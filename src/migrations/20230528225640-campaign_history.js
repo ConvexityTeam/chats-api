10415a91b2f413d658ebd03932aeae3cf4d54006
marwacoder <jibrilmohammed39@gmail.com>
2023-05-29 00:09:33 +0100

    added culumn version_history to campaign

---
+++ src/migrations/20230528225640-campaign_history.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Campaigns', 'version_history', {
+      type: Sequelize.JSON,
+      after: 'status',
+      defaultValue: false
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('Campaigns', 'version_history');
+  }
+};
