d56dde40b25df764240ff035c34afa2e26011aab
Habeeb Salami <talk2hb1@gmail.com>
2023-06-05 16:42:06 +0100

    plans and sub init

---
+++ src/controllers/SubscriptionsController.js
@@ -0,0 +1,39 @@
+class SubscriptionsControllers {
+  static async createSubscriptions(req, res) {
+    try {
+      const plan = await Subscriptions.create(req.body);
+      res.status(201).json(plan);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  }
+  static async getAllSubscriptions(req, res) {
+    try {
+      const Subscriptions = await Subscriptions.getAllSubscriptions();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  static async getSubscriptions(req, res) {
+    try {
+    } catch (error) {}
+  }
+  static async getSubscriptions(req, res) {
+    try {
+    } catch (error) {}
+  }
+  static async updateSubscriptions(req, res) {
+    try {
+        
+    } catch (error) {
+        
+    }
+  }
+  static async deleteSubscriptions(req, res) {
+    try {
+        
+    } catch (error) {
+        
+    }
+  }
+}
