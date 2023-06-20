c3519ae7a08931eaadc1d0ec0b1f7e39accda40f
Habeeb Salami <1849920+habeeb-salami@users.noreply.github.com>
2023-06-20 18:43:40 +0100

    currency services refactored for speed and efficiency

--- src/services/CurrencyServices.js
+++ src/services/CurrencyServices.js
@@ -4,27 +4,41 @@ const {default: axios} = require('axios');
 class CurrencyServices {
   httpService;
   appId;
+  exchangeData;
   constructor() {
     // this.httpService = 'https://openexchangerates.org/api';
     // this.appId = process.env.OPEN_EXCHANGE_APP;
+    this.exchangeData = this.getExchangeRate();
   }
 
-  static async getExchangeRate() {
+  async getExchangeRate() {
+    return await this.getExchangeRate();
+  }
+  async getExchangeRate() {
     // const appId = process.env.OPEN_EXCHANGE_APP;
     // console.log(appId);
     const url = `https://openexchangerates.org/api/latest.json?app_id=da41a176c0874c4498594d728d2aa4ca`;
     const exchange = await axios.get(url);
-    // console.log(exchange.data.rates);
-    return exchange.data.rates;
+    this.exchangeData = exchange.data.rates;
+    return this.exchangeData;
   }
 
-  static async convertCurrency(from, to, amount) {
-    const params = {params: {app_id: 'da41a176c0874c4498594d728d2aa4ca'}};
-    const url = `https://openexchangerates.org/api/convert/${amount}/${from}/${to}`;
-    let converted = await axios.get(url, params);
-    console.log(converted.data);
-    return converted;
+  async convertCurrency(fromCurrency, toCurrency, amount) {
+    const data = await this.getExchangeRate();
+    const currencies = Object.entries(data);
+    // get rate of from origin currency
+    const fromRate = currencies.find(row => {
+      return row[0] == fromCurrency;
+    });
+    // get rate of to destination currency
+    const toRate = currencies.find(row => {
+      return row[0] == toCurrency;
+    });
+    return this.convertRate(fromRate[1], toRate[1], amount);
+  }
+  async convertRate(fromRate, toRate, amount) {
+    return ((toRate / fromRate) * amount).toFixed(2);
   }
 }
 
-module.exports = CurrencyServices;
+module.exports = new CurrencyServices();
