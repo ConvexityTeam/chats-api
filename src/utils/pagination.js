35ec553037dd0abe6c4fabd207401891f400aee8
marwacoder <jibrilmohammed39@gmail.com>
2023-06-21 22:11:08 +0100

    page

--- src/utils/pagination.js
+++ src/utils/pagination.js
@@ -1,13 +1,13 @@
 class Pagination {
   static async getPagination(page, size) {
     const limit = size ? +size : 1;
-    const offset = page ? page * limit : 0;
+    const offset = (page - 1) * size;
 
     return {limit, offset};
   }
   static async getPagingData(details, page, limit) {
     const {count: totalItems, rows: data} = details;
-    const currentPage = page ? +page : 0;
+    const currentPage = page ? +page : 1;
     const totalPages = Math.ceil(totalItems / limit);
 
     return {totalItems, data, totalPages, currentPage};
