2c962af1e6f5f8f87ebbccffc14c5a611169c414
marwacoder <jibrilmohammed39@gmail.com>
2023-07-19 14:25:33 +0100

    fixed request password

--- src/validators/AuthValidator.js
+++ src/validators/AuthValidator.js
@@ -1,73 +1,88 @@
-const { Response } = require('../libs');
-const { body } = require('express-validator');
-const { isEmail } = require('validator')
-const { HttpStatusCode, compareHash } = require('../utils')
-const { UserService, AuthService } = require('../services');
+const {Response} = require('../libs');
+const {body} = require('express-validator');
+const {isEmail} = require('validator');
+const {HttpStatusCode, compareHash} = require('../utils');
+const {UserService, AuthService} = require('../services');
 const BaseValidator = require('./BaseValidator');
-const { SuperAdmin, GodMode, NgoAdmin, Vendor, Beneficiary } = require('../utils').AclRoles;
+const {SuperAdmin, GodMode, NgoAdmin, Vendor, Beneficiary, Donor} =
+  require('../utils').AclRoles;
 
 class AuthValidator extends BaseValidator {
-  static selfResetAllowedFor = [SuperAdmin, GodMode, NgoAdmin, Vendor, Beneficiary];
+  static selfResetAllowedFor = [
+    SuperAdmin,
+    Donor,
+    GodMode,
+    NgoAdmin,
+    Vendor,
+    Beneficiary
+  ];
 
   static requestPasswordResetRules() {
     return [
       body('email')
-      .custom((val) => isEmail(val))
-      .withMessage(`Email is not well formed.`)
-      .optional({
-        nullable: true,
-        checkFalsy: true
-      }),
+        .custom(val => isEmail(val))
+        .withMessage(`Email is not well formed.`)
+        .optional({
+          nullable: true,
+          checkFalsy: true
+        }),
       body('phone')
-      .isMobilePhone()
-      .withMessage(`Phone number is not well formed.`)
-      .optional({
-        nullable: true,
-        checkFalsy: true
-      })
-    ]
+        .isMobilePhone()
+        .withMessage(`Phone number is not well formed.`)
+        .optional({
+          nullable: true,
+          checkFalsy: true
+        })
+    ];
   }
 
   static resetPasswordRules() {
     return [
       body('otp')
-      .not().isEmpty()
-      .withMessage('Password reset token is required.'),
-      body('password')
-      .not().isEmpty()
-      .withMessage('Password is required.'),
+        .not()
+        .isEmpty()
+        .withMessage('Password reset token is required.'),
+      body('password').not().isEmpty().withMessage('Password is required.'),
       body('confirm_password')
-      .not().isEmpty()
-      .withMessage('Password confirmation is required.')
-      .custom((val, {
-        req
-      }) => {
-        return val == req.body.password
-      })
-      .withMessage('Password confirmation does not match.')
-    ]
+        .not()
+        .isEmpty()
+        .withMessage('Password confirmation is required.')
+        .custom((val, {req}) => {
+          return val == req.body.password;
+        })
+        .withMessage('Password confirmation does not match.')
+    ];
   }
 
   static async canResetPassword(req, res, next) {
     const query = {};
-    const { email, phone } = req.body;
-    email && Object.assign(query, { email });
-    phone && Object.assign(query, { phone });
+    const {email, phone} = req.body;
+    email && Object.assign(query, {email});
+    phone && Object.assign(query, {phone});
 
     if (!query.phone && !query.email) {
-      Response.setError(HttpStatusCode.STATUS_UNPROCESSABLE_ENTITY, 'Email or Phone number is required.');
+      Response.setError(
+        HttpStatusCode.STATUS_UNPROCESSABLE_ENTITY,
+        'Email or Phone number is required.'
+      );
       return Response.send(res);
     }
 
     const user = await UserService.findSingleUser(query);
 
     if (!user) {
-      Response.setError(HttpStatusCode.STATUS_RESOURCE_NOT_FOUND, 'Account not found.');
+      Response.setError(
+        HttpStatusCode.STATUS_RESOURCE_NOT_FOUND,
+        'Account not found.'
+      );
       return Response.send(res);
     }
 
     if (!AuthValidator.selfResetAllowedFor.includes(user.RoleId)) {
-      Response.setError(HttpStatusCode.STATUS_BAD_REQUEST, 'Reset password request not allowed for the user account.');
+      Response.setError(
+        HttpStatusCode.STATUS_BAD_REQUEST,
+        'Reset password request not allowed for the user account.'
+      );
       return Response.send(res);
     }
 
@@ -77,22 +92,32 @@ class AuthValidator extends BaseValidator {
 
   static async checkResetPasswordToken(req, res, next) {
     const reference = req.body.ref;
-    const record = reference && await AuthService.getPasswordTokenRecord(reference);
+    const record =
+      reference && (await AuthService.getPasswordTokenRecord(reference));
     if (!record) {
-      Response.setError(HttpStatusCode.STATUS_BAD_REQUEST, 'Invalid or expired request. Please try again');
+      Response.setError(
+        HttpStatusCode.STATUS_BAD_REQUEST,
+        'Invalid or expired request. Please try again'
+      );
       if (!reference) {
-        Response.setError(HttpStatusCode.STATUS_BAD_REQUEST, 'Invalid or missing reset password reference.');
+        Response.setError(
+          HttpStatusCode.STATUS_BAD_REQUEST,
+          'Invalid or missing reset password reference.'
+        );
       }
       return Response.send(res);
     }
 
     if (!compareHash(req.body.otp, record.token)) {
-      Response.setError(HttpStatusCode.STATUS_BAD_REQUEST, 'Invalid or wrong OTP.');
+      Response.setError(
+        HttpStatusCode.STATUS_BAD_REQUEST,
+        'Invalid or wrong OTP.'
+      );
       return Response.send(res);
     }
 
     req.user = await record.getUser();
-    next()
+    next();
   }
 }
 
