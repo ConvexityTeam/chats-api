63dee15382288d235df6c906656bdb71b2b2f814
jesudara jeminusi <jesudara.j@gmail.com>
2023-07-27 19:57:16 +0100

    currency data changes

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1163,6 +1163,9 @@ class AuthController {
       );
       const data = await AuthService.login(user, req.body.password);
       data.user.dataValues.currencyData = currencyData;
+      console.log("data", data.user.dataValues.currencyData);
+      console.log("data", currencyData);
+      
       Response.setSuccess(200, 'Login Successful.', data);
       return Response.send(res);
     } catch (error) {
