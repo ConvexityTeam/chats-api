48bbece606387a4c42faa449c06989389c717657
jesudara jeminusi <jesudara.j@gmail.com>
2023-08-06 19:53:39 +0100

    order update

--- src/consumers/TransactionConsumer.js
+++ src/consumers/TransactionConsumer.js
@@ -1669,12 +1669,12 @@ RabbitMq['default']
           msg.nack();
           return;
         }
-        await update_transaction(
-          {
-            transaction_hash: gasFee.retried
-          },
-          transactionId
-        );
+        // await update_transaction(
+        //   {
+        //     transaction_hash: gasFee.retried
+        //   },
+        //   transactionId
+        // );
         await QueueService.confirmVendorOrder(
           gasFee.retried,
           amount,
@@ -1738,7 +1738,11 @@ RabbitMq['default']
 
         await blockchainBalance(balance, vendorWallet.uuid);
         await update_transaction(
-          {status: 'success', is_approved: true},
+          {
+            transaction_hash: hash,
+            status: 'success',
+            is_approved: true
+          },
           transactionId
         );
         order.Cart.forEach(async prod => {
