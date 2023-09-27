21769a6cae0b30b7bf71aca98247dca6781ab2f0
marwacoder <jibrilmohammed39@gmail.com>
2023-09-27 23:45:05 +0100

    fix: complain

--- src/controllers/ComplaintController.js
+++ src/controllers/ComplaintController.js
@@ -7,26 +7,24 @@ class ComplaintController {
     try {
       const filter = SanitizeObject(req.query, ['status']);
       const campaign = await CampaignService.getPubCampaignById(
-        req.params.campaign_id,
+        req.params.campaign_id
       );
-      const {
-        count: complaints_count,
-        rows: Complaints,
-      } = await ComplaintService.getCampaignComplaints(
-        req.params.campaign_id,
-        filter,
-      );
+      const {count: complaints_count, rows: Complaints} =
+        await ComplaintService.getCampaignComplaints(
+          req.params.campaign_id,
+          filter
+        );
       Response.setSuccess(HttpStatusCode.STATUS_OK, 'Campaign Complaints.', {
         ...campaign,
         complaints_count,
-        Complaints,
+        Complaints
       });
       return Response.send(res);
     } catch (error) {
       console.log(error);
       Response.setError(
         HttpStatusCode.STATUS_INTERNAL_SERVER_ERROR,
-        `Internal server error. Contact support.`,
+        `Internal server error. Contact support.`
       );
       return Response.send(res);
     }
@@ -35,24 +33,20 @@ class ComplaintController {
     try {
       const filter = SanitizeObject(req.query, ['status']);
       const campaign = req.campaign.toJSON();
-      const {
-        count: complaints_count,
-        rows: Complaints,
-      } = await ComplaintService.getCampaignComplaints(
+      const complaint = await ComplaintService.getCampaignComplaints(
         req.params.campaign_id,
-        filter,
+        filter
       );
       Response.setSuccess(HttpStatusCode.STATUS_OK, 'Campaign Complaints.', {
         ...campaign,
-        complaints_count,
-        Complaints,
+        complaint
       });
       return Response.send(res);
     } catch (error) {
       console.log(error);
       Response.setError(
         HttpStatusCode.STATUS_INTERNAL_SERVER_ERROR,
-        `Internal server error. Contact support.`,
+        `Internal server error. Contact support.`
       );
       return Response.send(res);
     }
@@ -61,19 +55,19 @@ class ComplaintController {
   static async getCampaignConplaint(req, res) {
     try {
       const complaint = await ComplaintService.getComplaint(
-        req.params.complaint_id,
+        req.params.complaint_id
       );
       Response.setSuccess(
         HttpStatusCode.STATUS_OK,
         'Campaign Complaint.',
-        complaint,
+        complaint
       );
       return Response.send(res);
     } catch (error) {
       console.log(error);
       Response.setError(
         HttpStatusCode.STATUS_INTERNAL_SERVER_ERROR,
-        `Internal server error. Contact support.`,
+        `Internal server error. Contact support.`
       );
       return Response.send(res);
     }
@@ -82,21 +76,21 @@ class ComplaintController {
   static async resolveCampaignConplaint(req, res) {
     try {
       await ComplaintService.updateComplaint(req.params.complaint_id, {
-        status: 'resolved',
+        status: 'resolved'
       });
       const complaint = req.complaint.toJSON();
       complaint.status = 'resolved';
       Response.setSuccess(
         HttpStatusCode.STATUS_OK,
         'Campaign Complaint Resolved.',
-        complaint,
+        complaint
       );
       return Response.send(res);
     } catch (error) {
       console.log(error);
       Response.setError(
         HttpStatusCode.STATUS_INTERNAL_SERVER_ERROR,
-        `Internal server error. Contact support.`,
+        `Internal server error. Contact support.`
       );
       return Response.send(res);
     }
