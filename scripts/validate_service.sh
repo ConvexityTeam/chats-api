48dcdb6b750746019262da87e22faa217669361f
convexitygeeks <144256722+convexitygeeks@users.noreply.github.com>
2023-12-06 18:40:14 +0100

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -2,15 +2,16 @@
 
 set -e
 
+cd /var/www/app
 # Perform any validation checks here, e.g., health checks, connectivity checks, etc.
 
 # Example: Check if a service is running
-# if ! docker-compose ps -q | grep -q backend; then
-#     echo "Error: The required service is not running."
-#     exit 1
-# fi
+if ! docker-compose ps -q | grep -q backend; then
+    echo "Error: The required service is not running."
+    exit 1
+fi
 
-# if ! docker-compose ps -q | grep -q compose; then
-#     echo "Error: The required service is not running."
-#     exit 1
-# fi
+if ! docker-compose ps -q | grep -q compose; then
+    echo "Error: The required service is not running."
+    exit 1
+fi
