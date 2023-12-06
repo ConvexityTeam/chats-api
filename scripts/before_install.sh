df6847fd30e527460f74e72782c0ae044fd9ec4c
convexitygeeks <144256722+convexitygeeks@users.noreply.github.com>
2023-12-06 15:47:33 +0100

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,7 +2,5 @@
 
 set -e
 
-echo "next listing"
-ls
 # Stop and remove existing Docker Compose services and containers
-# docker-compose down --volumes --remove-orphans
+docker-compose down -f ./docker-compose.yml --volumes --remove-orphans
