bac27580ff99f2e2d6352c865929d0b75a5a2f2c
convexitygeeks <144256722+convexitygeeks@users.noreply.github.com>
2023-12-07 09:56:56 +0100

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 set -e
-chmod +w /var/www
+# chmod +w /var/www
 cd /var/www/app
 
 # Build and start Docker Compose services
-docker-compose build
+# docker-compose build
 docker-compose up -d
