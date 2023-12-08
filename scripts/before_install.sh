45bdba80ed81a85af1a9a8a6dec10a2231fc96c2
convexitygeeks <144256722+convexitygeeks@users.noreply.github.com>
2023-12-08 05:06:02 +0100

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-chmod +w /var/www/app
+# chmod +w /var/www/app
 
 cd /var/www/app
 # Stop and remove existing Docker Compose services and containers
-docker-compose down --volumes --remove-orphans
+sudo docker-compose down --volumes --remove-orphans
