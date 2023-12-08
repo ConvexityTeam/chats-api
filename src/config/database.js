0907f1679ff85a05cdbf9fcc283e4c6dd29ff8cc
convexitygeeks <144256722+convexitygeeks@users.noreply.github.com>
2023-12-08 06:46:45 +0100

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const config = {
   DB_NAME: process.env.DB_NAME,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_HOST: process.env.DB_HOST,
+  DB_HOST: "staging-db.chats.cash",
   DB_PORT: process.env.DB_PORT,
   DB_LOG: process.env.QUERY_LOGGING || false
 };
