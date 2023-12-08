06684f9b099d98701826ff60620e07a86994f1bd
convexitygeeks <144256722+convexitygeeks@users.noreply.github.com>
2023-12-08 17:05:05 +0100

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -7,15 +7,15 @@ set -e
 # # Download Docker image from S3
 # aws s3 cp s3://chats-docker/backend.tar.gz /var/www/app/backend.tar.gz
 # aws s3 cp s3://chats-docker/consumer.tar.gz /var/www/app/consumer.tar.gz
-# aws s3 cp s3://chats-docker/chatsapi.tar /var/www/app/chatsapi.tar
+aws s3 cp s3://chats-docker/chatsapi.tar /var/www/app/chatsapi.tar
 
 
-cd /var/www/app
-aws s3 cp s3://chats-docker/backend.tar .
-aws s3 cp s3://chats-docker/consumer.tar .
+# cd /var/www/app
+# aws s3 cp s3://chats-docker/backend.tar .
+# aws s3 cp s3://chats-docker/consumer.tar .
 
-docker load -i backend.tar
-docker load -i consumer.tar
+# docker load -i backend.tar
+# docker load -i consumer.tar
 
 docker-compose up -d
 
@@ -24,5 +24,6 @@ docker-compose up -d
 # docker load -i consumer.tar.gz
 # docker run -d backend
 # # docker run -d consumer
-# docker load -i chatsapi.tar
+docker load -i chatsapi.tar
 # docker run -d chatsapi
+docker-compose up -d
