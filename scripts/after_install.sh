59169ee9c387b0483c210bcdc651ac0ac32f3a78
convexitygeeks <144256722+convexitygeeks@users.noreply.github.com>
2023-12-08 08:31:36 +0100

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,4 +6,10 @@ cd /var/www/app
 
 # Build and start Docker Compose services
 # docker-compose build
+
+# Download Docker image from S3
+aws s3 cp s3://chats-docker/chatsapi.tar.gz /var/www/app/chatsapi.tar.gz
+
+# Load Docker image
+docker load -i chatsapi.tar.gz
 docker-compose up -d
